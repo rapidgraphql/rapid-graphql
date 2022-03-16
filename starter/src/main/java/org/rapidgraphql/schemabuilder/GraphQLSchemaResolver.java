1862646e9fc24bfdda8f6a5dda83f8df9ba32e9a
mgroys <mgroys@ebay.com>
2022-03-17 01:34:04 +0200

    fixed spring factories location

--- starter/src/main/java/org/rapidgraphql/schemabuilder/GraphQLSchemaResolver.java
+++ starter/src/main/java/org/rapidgraphql/schemabuilder/GraphQLSchemaResolver.java
@@ -1,5 +1,6 @@
 package org.rapidgraphql.schemabuilder;
 
+import graphql.kickstart.autoconfigure.tools.GraphQLJavaToolsAutoConfiguration;
 import graphql.kickstart.tools.GraphQLResolver;
 import graphql.kickstart.tools.SchemaParser;
 import graphql.kickstart.tools.SchemaParserOptions;
@@ -10,6 +11,7 @@ import graphql.scalars.ExtendedScalars;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,15 +21,16 @@ import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+@AutoConfigureBefore(GraphQLJavaToolsAutoConfiguration.class)
 @Configuration
 public class GraphQLSchemaResolver {
     private static final Logger LOGGER = getLogger(GraphQLSchemaResolver.class);
-    @Autowired
-    List<? extends GraphQLResolver<?>> resolvers;
+    private final List<? extends GraphQLResolver<?>> resolvers;
 
     private final DefinitionFactory definitionFactory;
 
-    public GraphQLSchemaResolver() {
+    public GraphQLSchemaResolver(List<? extends GraphQLResolver<?>> resolvers) {
+        this.resolvers = resolvers;
         definitionFactory = new DefinitionFactory(new DefaultValueAnnotationProcessorImpl());
     }
 
