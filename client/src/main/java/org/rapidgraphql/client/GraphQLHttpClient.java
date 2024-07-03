ddc3fe174cf79d38fc419b78c4d30c50e22bb409
Michael Groys <mishagr@users.noreply.github.com>
2024-07-03 18:42:41 +0300

    Version 2.2.3 * Added content-type to graphql client * Updated version of unirest * support of additional types

--- client/src/main/java/org/rapidgraphql/client/GraphQLHttpClient.java
+++ client/src/main/java/org/rapidgraphql/client/GraphQLHttpClient.java
@@ -5,16 +5,18 @@ import lombok.Builder;
 import org.rapidgraphql.client.exceptions.GraphQLHttpErrorException;
 import org.rapidgraphql.client.extractor.ResultExtractor;
 
-@Builder
 public class GraphQLHttpClient {
     private final String url;
-    private final Config requestConfig;
+    UnirestInstance unirestInstance;
+    @Builder
+    public GraphQLHttpClient(String url, Config requestConfig) {
+        this.url = url;
+        this.unirestInstance = new UnirestInstance(requestConfig);
+    }
     public Object exchange(GraphQLRequestBody graphQLRequestBody, ResultExtractor extractor) {
-        HttpRequestWithBody requestWithBody = Unirest.post(url)
-                .accept("application/json");
-        if (requestConfig != null) {
-            requestWithBody = requestWithBody.connectTimeout(requestConfig.getConnectionTimeout());
-        }
+        HttpRequestWithBody requestWithBody = unirestInstance.post(url)
+                .accept("application/json")
+                .contentType("application/json");
         HttpResponse<JsonNode> jsonNodeHttpResponse = requestWithBody
                 .body(graphQLRequestBody)
                 .headers(graphQLRequestBody.getHeaders())
--- client/src/main/java/org/rapidgraphql/client/RapidGraphQLClient.java
+++ client/src/main/java/org/rapidgraphql/client/RapidGraphQLClient.java
@@ -1,29 +1,40 @@
 package org.rapidgraphql.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import kong.unirest.core.Config;
+import kong.unirest.modules.jackson.JacksonObjectMapper;
 
 import java.lang.reflect.Proxy;
 
 public class RapidGraphQLClient {
     public static class Builder {
-        private ObjectMapper objectMapper;
-        private Config requestConfig;
+        private static final ObjectMapper DEFAULT_OBJECT_MAPPER = JsonMapper.builder() // or different mapper for other format
+                .addModule(new ParameterNamesModule())
+                .addModule(new Jdk8Module())
+                .addModule(new JavaTimeModule())
+                .build();
+        private ObjectMapper objectMapper = DEFAULT_OBJECT_MAPPER;
+        private final Config requestConfig = new Config().setObjectMapper(new JacksonObjectMapper(DEFAULT_OBJECT_MAPPER));
 
         public Builder objectMapper(ObjectMapper objectMapper) {
             this.objectMapper = objectMapper;
+            requestConfig.setObjectMapper(new JacksonObjectMapper(objectMapper));
             return this;
         }
         public Builder connectTimeoutMs(int millies) {
-            if (requestConfig == null) {
-                requestConfig = new Config();
-            }
             requestConfig.connectTimeout(millies);
+            return this;
+        }
+        public Builder requestTimeoutMs(int millies) {
+            requestConfig.requestTimeout(millies);
             return this;
         }
         public <T> T target(Class<T> apiClass, String url) {
             GraphQLHttpClient graphQLHttpClient = new GraphQLHttpClient(url, requestConfig);
-            ObjectMapper objectMapper = this.objectMapper==null ? new ObjectMapper() : this.objectMapper;
             return (T) Proxy.newProxyInstance(
                     apiClass.getClassLoader(),
                     new Class[]{apiClass},
